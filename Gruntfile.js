ab344c479c922d6d86acf8cea58b23b6cd22c6f3
Arminlife <ppbamzzi@gmail.com>
2016-01-11 23:12:49 +0200

    release dev

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: [
+          'less/modules/mixins.less',
+          'less/modules/base.less',
+          'less/modules/buttons.less',
+          'less/modules/section.less',
+          'less/modules/header.less',
+          'less/modules/titles.less',
+          'less/modules/big-slider.less',
+          'less/modules/icon-boxes.less',
+          'less/modules/lists.less'
+        ],
+        dest: 'less/style.less',
+      },
+    },
+    less: {
+      development: {
+        files: {
+          "css/style.css": "less/style.less"
+        }
+      }
+    },
+    watch: {
+      files: ['less/modules/*.less'],
+      tasks: ['concat','less']
+    },
+    browserSync: {
+      dev: {
+          bsFiles: {
+              src : [
+                  'css/style.css',
+                  '*.html'
+              ]
+          },
+          options: {
+              watchTask: true,
+              server: './'
+          }
+      }
+    }
+
+  });
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+  // Default task(s).
+  grunt.registerTask('default', ['browserSync', 'watch']);
+
+};
